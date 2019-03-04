83de1441f8ba3f1aa2e38cdb47c577a4b8a67a1f
David Ng <davidng@hk01.com>
2019-03-04 17:38:02 +0800

    fixup! Feat/ajv (#2)

--- index.d.ts
+++ index.d.ts
@@ -1,28 +1,26 @@
-import * as sway from "sway"
-import * as ajv from "ajv"
+import * as ajv from "ajv";
+import * as express from "express";
+import * as sway from "sway";
 
-interface CreateMiddlewareOptions {
-  swayOptions?: sway.CreateOptions,
-  swayValidateRequestOptions?: sway.RequestValidationOptions,
-  swayValidateResponseOptions?: sway.ResponseValidationOptions,
-  ajvOptions?: ajv.Options,
+export interface CreateMiddlewareOptions {
+  swayOptions?: sway.CreateOptions;
+  swayValidateRequestOptions?: sway.RequestValidationOptions;
+  swayValidateResponseOptions?: sway.ResponseValidationOptions;
+  ajvOptions?: ajv.Options;
   middlewareOptions?: {
-    strictMode: boolean
-  }
+    strictMode: boolean;
+  };
 }
-
-declare namespace SwaggerMiddleware {
-  function createExpressMiddleware(swaggerIndexFile: string, options: CreateMiddlewareOptions): Function;
-  class ValidationError extends Error{
-    errors: Array<sway.ValidationEntry>
-  }
-  class AJVValidationError extends Error{
-    errors: Array<ajv.ErrorObject>
-  }
-  class SpecNotFoundError extends Error {}
-
+export class ValidationError extends Error {
+  public errors: sway.ValidationEntry[];
+  constructor(swayError: sway.ValidationResults)
 }
-
-declare module "node-swagger-middleware" {
-  export = SwaggerMiddleware;
+export class AJVValidationError extends Error {
+  public errors: ajv.ErrorObject[];
+  constructor(ajvError: ajv.ErrorObject[]);
 }
+export class SpecNotFoundError extends Error {
+  constructor(method: string, path: string)
+}
+export function createExpressMiddleware(swaggerIndexFile: string, options: CreateMiddlewareOptions):
+(req: express.Request, res: express.Response, next: express.NextFunction) => Promise<void>;
